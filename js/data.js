89f061e9f15c6af79efd97c1a769c0666f272103
Penpen7 <Raosys@live.jp>
2016-03-24 13:21:20 +0900

    second commit

---
+++ js/data.js
@@ -0,0 +1,15 @@
+var codropsEvents = {
+	// 形式 '99-99-9999' : 'hogehoge'
+	'TODAY' : '<span>Today, [Date : \'TODAY\']</span>',
+	'04-10-2016' : '<div class="cherry">花見<br>@鴨川</div>',
+	'04-11-2016' : '<div class="present">ぷち説明会<br>@4共13</div>',
+	'04-13-2016' : '<div class="present">ぷち説明会<br>@4共11</div>',
+	'04-19-2016' : '<div class="present">ぷち説明会<br>@4共20</div>',
+	'04-21-2016' : '<div class="present">ぷち説明会<br>@4共20</div>',
+	'04-26-2016' : '<div class="present">ぷち説明会<br>(未定)</div>',
+	'04-20-2016' : '<div class="demo">デモ演奏<br>@吉田食堂前</div>',
+	'04-16-2016' : '<div class="see">練習見学会<br>@ 出町センター</div>',
+	'04-22-2016' : '<div class="try">体験会<br>@ 出町センター</div>',
+	'04-24-2016' : '<div>春ライブ<br>@ アバンティ</div>',
+	'04-30-2016' : '入会〆切'
+};
